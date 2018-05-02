ca8d545c143d0c5222e612fa2345428cb66d0332
Andy <yihuiemail@163.com>
2018-05-02 22:31:59 +0800

    add button envent in manualset.xaml

--- CoalMonitor/windows/ManualSet.xaml.cs
+++ CoalMonitor/windows/ManualSet.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using CoalMonitor.classlib;
 
 namespace CoalMonitor.windows
 {
@@ -24,6 +25,33 @@ namespace CoalMonitor.windows
             InitializeComponent();
             //List<String> myList = new List<String>{ "1", "2", "3", "4" };
             //listBox.ItemsSource = myList;
+        }
+
+        private void listBox_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            AixParameter para = (AixParameter)btn.DataContext;
+            switch (para.Name)
+            {
+                case "1轴":
+                    MessageBox.Show("1轴");
+                    break;
+                case "2轴":
+                    MessageBox.Show("2轴");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void DownButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
